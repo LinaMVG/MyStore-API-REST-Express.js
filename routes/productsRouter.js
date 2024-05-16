b1994c19771639ccf197c2a46ef6ae30eaf2e6d5
LinaMVG <limarcegarzon@gmail.com>
2024-05-16 17:23:38 -0500

    finalizando CRUD y aplicando statusCode

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -24,10 +24,53 @@ router.get('/filter', (req, res)=>{
 
 router.get('/:id', (req,res) =>{
   const id= req.params.id // const {id}=req.params // y se devuelve en el res.json
+  if(id == '999'){
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  } else{
+    res.status(200).json({
+      id,
+      name: 'producto 1',
+      price : 1000
+    })
+  }
+
+})
+
+router.post('/', (req,res) =>{
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  })
+})
+
+router.put('/:id', (req,res) =>{
+  const {id} = req.params;
+  const body = req.body;
   res.json({
-    id,
-    name: 'producto 1',
-    price : 1000
+    message: 'updated',
+    data: body,
+    id
+  })
+})
+
+router.patch('/:id', (req,res) =>{
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'updated partial',
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id', (req,res) =>{
+  const {id} = req.params;
+  res.json({
+    message: 'deleted',
+    id
   })
 })
 
