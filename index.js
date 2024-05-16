4b7925422c751df00c5bcc8ee1dc2d2721754c89
LinaMVG <limarcegarzon@gmail.com>
2024-05-16 15:45:02 -0500

    Servidor Http, instalacion Express.js, routing con Express.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('My App');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res)=>{
+  res.send('Hola, mi server en express')
+})
+
+app.get('/nueva-ruta', (req,res)=>{
+  res.send('Hola, soy el nuevo endpoint')
+})
+
+app.get('/products', (req,res)=>{
+  res.json({
+    name: 'producto 1',
+    price : 1000
+  })
+})
+
+app.listen(port, ()=>{
+  console.log('Escuchando en el puerto: ' + port);
+})
+
