269249910bdfee33ac72ef47b0acd1b7f195194c
LinaMVG <limarcegarzon@gmail.com>
2024-05-23 16:38:30 -0500

    modelos  ORM

--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,8 @@ const boom = require('@hapi/boom');
 const getConnection = require('../libs/postgres');
 const pool = require('../libs/postgresPool');
 
+const {models} = require('./../libs/sequelize')
+
 class UserService {
   constructor() {
     this.pool = pool;
@@ -14,10 +16,15 @@ class UserService {
   }
 
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const rta = await models.User.findAll();
+    return rta;
 
+    //2
+    // const query = 'SELECT * FROM tasks';
+    // const rta = await this.pool.query(query);
+    // return rta.rows;
+
+    //1
     // const client = await getConnection();
     // const rta = await client.query('SELECT * FROM tasks');
     // return rta.rows;
