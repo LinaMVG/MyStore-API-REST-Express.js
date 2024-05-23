ab5260c06da827e11477663236593577816167f3
LinaMVG <limarcegarzon@gmail.com>
2024-05-23 15:34:36 -0500

    pool para mejorar la conexion

--- services/productService.js
+++ services/productService.js
@@ -1,11 +1,15 @@
 const {faker} = require('@faker-js/faker')
 const boom = require('@hapi/boom')
 
+const pool = require('../libs/postgresPool');
+
 class ProductsService{
 
   constructor(){
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   generate(){
@@ -30,14 +34,19 @@ class ProductsService{
     return newProduct
   }
 
-  find(){
+  async find(){
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
+
     // return new Promise((resolve, reject)=>{
     //   setTimeout(() => {
     //     resolve(this.products)
 
     //   }, 5000);
     // })
-    return this.products
+
+    // return this.products
   }
 
   async findOne(id){
